369379bf291886c98c23da6a18e053d5529eb6c5
Lorenz Breidenbach <lorenzb@eth.edu>
2017-10-02 11:33:30 +0200

    Make everything relative to run.sh script

--- run.sh
+++ run.sh
@@ -2,23 +2,23 @@
 
 set -euo pipefail
 
-declare -r DATADIR=$HOME/geth-myrtle
+cd "$(dirname "$0")"
 
-if [ -d "$DATADIR/geth" ]
+if [ -d "geth" ]
 then
 	echo "There seems to be a preexisting chain. Do you want to start a new one?"
 	select yn in "No" "Yes"
 	do
 		case $yn in
-			Yes ) rm -r "$DATADIR/geth" && echo "Deleted existing chain"; break;;
+			Yes ) rm -r geth && echo "Deleted existing chain"; break;;
 			No ) echo "Keeping existing chain"; break;;
 		esac
 	done
 fi
 
-if ! [ -d "$DATADIR/geth" ]
+if ! [ -d "geth" ]
 then
-	geth --datadir "$DATADIR" init genesis.json
+	geth --datadir . init genesis.json
 fi
 
-geth --datadir "$DATADIR" --networkid 7418 --rpc --rpccorsdomain '*' --rpcapi "eth,net,web3,debug" --unlock 0,1,2 --password <(echo;echo;echo;echo) --mine --minerthreads=1
+geth --datadir . --networkid 7418 --rpc --rpccorsdomain '*' --rpcapi "eth,net,web3,debug" --unlock 0,1,2 --password <(echo;echo;echo;echo) --mine --minerthreads=1
